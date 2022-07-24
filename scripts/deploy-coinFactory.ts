b324f6a16bdb83af0ac17de5b8bc9f58efd3b180
friday-james <lamcollin37@gmail.com>
2022-07-24 16:22:47 -0700

    Run linter

--- scripts/deploy-coinFactory.ts
+++ scripts/deploy-coinFactory.ts
@@ -1,34 +1,39 @@
-import { ethers, network } from "hardhat";
+import { ethers, network } from 'hardhat'
 
-const config = require("../config");
-const currentNetwork = network.name;
+const config = require('../config')
+const currentNetwork = network.name
 
 async function main() {
-  if (currentNetwork == "mainnet") {
+  if (currentNetwork == 'mainnet') {
     if (!process.env.KEY_MAINNET) {
-      throw new Error("Missing private key, refer to README 'Deployment' section");
+      throw new Error(
+        "Missing private key, refer to README 'Deployment' section"
+      )
     }
     if (
       !config.Admin[currentNetwork] ||
-      config.Admin[currentNetwork] === "0x0000000000000000000000000000000000000000"
+      config.Admin[currentNetwork] ===
+        '0x0000000000000000000000000000000000000000'
     ) {
-      throw new Error("Missing admin address, refer to README 'Deployment' section");
+      throw new Error(
+        "Missing admin address, refer to README 'Deployment' section"
+      )
     }
   }
 
-  console.log("Deploying to network:", currentNetwork);
+  console.log('Deploying to network:', currentNetwork)
 
-  console.log("Deploying Coin Factory...");
+  console.log('Deploying Coin Factory...')
 
-  const CoinFactory = await ethers.getContractFactory("CoinFactory");
-  const coinFactory = await CoinFactory.deploy();
+  const CoinFactory = await ethers.getContractFactory('CoinFactory')
+  const coinFactory = await CoinFactory.deploy()
 
-  console.log("CoinFactory deployed to:", coinFactory.address);
+  console.log('CoinFactory deployed to:', coinFactory.address)
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
